e0ee8ca51da0ac5f04f45e8203467e290f90dd44
Riya Roy <riyaroy2086@gmail.com>
2023-04-27 21:55:41 +0530

    Updated - GFG, DAY-3

--- Strings/Implement Atoi.cpp
+++ Strings/Implement Atoi.cpp
@@ -14,37 +14,26 @@ class Solution{
 
 //-------------------------------------------------------------
 
-
-class Solution
-{
+class Solution{
   public:
-    long long int substrCount (string s, int k) {
-        return solve(s,k)-solve(s,k-1);  
-    }
-    
-    long long int solve(string &s, int k)
-    {
-        int c=0,freq[26]={0},n=s.size();
-        long long int res=0;
-        int i=0,j=0; // start and end of window
-
-        while(j<n)
+    /*You are required to complete this method */
+    int atoi(string str) {
+        int res=0,sign=1,i=0,n=str.size();
+        
+        if(str[0]=='-')
         {
-            freq[s[j]-'a']++; //increasing the freq
-            if( freq[s[j]-'a']==1)
-                c++;
-                
-            //Decreasing the size of window
-            while(c>k)
-            {
-                freq[s[i]-'a']--;
-                if(freq[s[i]-'a']==0) 
-                    c--;
-                i++;    
-            }
-            j++;
-            res=res+(j-i+1);
+            i=1;
+            sign=-1;
         }
-        return res;
+        
+        for(;i<n;i++)
+        {
+            int temp=str[i]-'0';// removing the zeros if any
+            //maintainfg non-no. char
+            if(temp>9 ||temp<0) return -1;
+            
+            res=res*10+temp;
+        }
+        return (sign*res);
     }
 };
