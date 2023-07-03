71ed95704730060d7f00c17d6721c49618e5e2ff
Riya Roy <riyaroy2086@gmail.com>
2023-07-03 18:11:22 +0530

    LC, MEDIUM

---
+++ DP/322. Coin_Change.cpp
@@ -0,0 +1,59 @@
+You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
+
+Return the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
+
+You may assume that you have an infinite number of each kind of coin.
+
+
+        
+Example 1:
+
+Input: coins = [1,2,5], amount = 11
+Output: 3
+Explanation: 11 = 5 + 5 + 1
+Example 2:
+
+Input: coins = [2], amount = 3
+Output: -1
+Example 3:
+
+Input: coins = [1], amount = 0
+Output: 0
+ 
+
+Constraints:
+
+1 <= coins.length <= 12
+1 <= coins[i] <= 231 - 1
+0 <= amount <= 104
+
+
+
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        //TC -> O(coins*amt)
+        //SC-> O(amt)
+        int n = coins.size();
+        vector<int> dp(amount+1);
+        dp[0]=0;
+
+        for(int i=1;i<=amount;++i)
+        {
+            dp[i] = INT_MAX; //Return the fewest number of coins
+            for(int j=0;j<n;++j)
+                if(coins[j]<=i)
+                {
+                    int res = dp[i-coins[j]];
+                    if(res!=INT_MAX && res+1 < dp[i])
+                        dp[i] = res+1;
+
+                }
+                //     dp[i] = min(dp[i], dp[i - coins[j]] + 1);
+                // Line 15: Char 57: runtime error: signed integer overflow: 2147483647 + 1 cannot be represented in type 'int' (solution.cpp)
+        }
+        return (dp[amount]>amount) ? -1:dp[amount];
+    }
+};
+
+//https://leetcode.com/problems/coin-change/submissions/846004038/
