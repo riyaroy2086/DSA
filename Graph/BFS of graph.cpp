04c7cc88064bfbd743b59b000e89125e76297077
Riya Roy <riyaroy2086@gmail.com>
2023-07-10 23:53:55 +0530

    Updated BFS of graph.cpp

--- Graph/BFS of graph.cpp
+++ Graph/BFS of graph.cpp
@@ -1,5 +1,5 @@
 Given a directed graph. The task is to do Breadth First Traversal of this graph starting from 0.
-Note: One can move from node u to node v only if there's an edge from u to v. Find the BFS traversal of the graph starting from the 0th vertex, from left to right according to the input graph. Also, you should only take nodes directly or indirectly connected from Node 0 in consideration.
+Note: One can move from node u to node v only if there\'s an edge from u to v. Find the BFS traversal of the graph starting from the 0th vertex, from left to right according to the input graph. Also, you should only take nodes directly or indirectly connected from Node 0 in consideration.
 
 
 Example 1:
