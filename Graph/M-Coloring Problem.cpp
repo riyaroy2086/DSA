91fa193d97376189bae43d5b0dba72ed1ed427e2
Riya Roy <riyaroy2086@gmail.com>
2023-07-15 23:20:19 +0530

    GFG

--- Graph/M-Coloring Problem.cpp
+++ Graph/M-Coloring Problem.cpp
@@ -80,3 +80,78 @@ public:
         return res;
     }
 };
+
+// Stack based solution gives TLE
+
+// ----------------------------------------------------------------SOLUTION : 2 -------------------------------------------------------------------- //
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+public:
+    // Function to determine if graph can be coloured with at most M colours such
+    // that no two adjacent vertices of graph are coloured with same colour.
+    bool graphColoring(bool graph[101][101], int m, int n) 
+    {
+        vector<int> color(n,0);
+   
+        if(func(0,color,graph,m,n)) return true;
+        
+        return false;
+    }
+    
+    bool func(int v,vector<int> &color,bool graph[101][101], int m, int n)
+    {
+        if(v == n) return true;
+        
+        for(int i=1;i<=m;++i)
+        {
+            if(check(v,color,graph,i,n))
+            {
+                color[v]=i;
+                if(func(v+1,color,graph,m,n)) return true;
+                color[v]=0;
+            }
+        }
+        return false;
+    }
+    bool check(int v,vector<int> &color,bool graph[101][101],int col,int n)
+    {
+        for(int i=0;i<n;++i)
+        {
+           if(i!=v && graph[i][v] == 1 && color[i] == col) return false;
+        }
+        return true;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m, e;
+        cin >> n >> m >> e;
+        int i;
+        bool graph[101][101];
+        for (i = 0; i < n; i++) {
+            memset(graph[i], 0, sizeof(graph[i]));
+        }
+        for (i = 0; i < e; i++) {
+            int a, b;
+            cin >> a >> b;
+            graph[a - 1][b - 1] = 1;
+            graph[b - 1][a - 1] = 1;
+        }
+        Solution ob;
+        cout << ob.graphColoring(graph, m, n) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
