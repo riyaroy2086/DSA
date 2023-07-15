dbc115c491e2c2a538654775791b76e0a8eaf70d
Riya Roy <riyaroy2086@gmail.com>
2023-07-15 22:31:40 +0530

    GFG

---
+++ DP/Longest Common Substring.cpp
@@ -0,0 +1,77 @@
+Given two strings. The task is to find the length of the longest common substring.
+
+
+Example 1:
+
+Input: S1 = "ABCDGH", S2 = "ACDGHR", n = 6, m = 6
+Output: 4
+Explanation: The longest common substring
+is "CDGH" which has length 4.
+Example 2:
+
+Input: S1 = "ABC", S2 "ACB", n = 3, m = 3
+Output: 1
+Explanation: The longest common substrings
+are "A", "B", "C" all having length 1.
+
+Your Task:
+You don't need to read input or print anything. Your task is to complete the function longestCommonSubstr() which takes the string S1, string S2 and their length n and m as inputs and returns the length of the longest common substring in S1 and S2.
+
+
+Expected Time Complexity: O(n*m).
+Expected Auxiliary Space: O(n*m).
+
+
+Constraints:
+1<=n, m<=1000
+
+// ------------------------------------------------SOLUTION------------------------------------------- //
+
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    public:
+    
+    int longestCommonSubstr (string S1, string S2, int n, int m)
+    {
+        vector<vector<int>> dp(n+1,vector<int> (m+1));
+        int maxlen = 0;
+        for(auto i : S1)
+        {
+            for(auto j : S2)
+            {
+                if(i == 0 && j == 0) dp[i][j] = 0;
+                else if (S1[n-1] == S2[m-1]) // if the char matches then
+                {
+                    dp[i][j] = dp[i-1][j-1]+1 ;
+                }
+                else dp[i][j] =0;
+                
+                maxlen = max(maxlen,dp[i][j]);
+            }
+        }
+        return maxlen;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t; cin >> t;
+    while (t--)
+    {
+        int n, m; cin >> n >> m;
+        string s1, s2;
+        cin >> s1 >> s2;
+        Solution ob;
+
+        cout << ob.longestCommonSubstr (s1, s2, n, m) << endl;
+    }
+}
+// Contributed By: Pranay Bansal
+
+// } Driver Code Ends
