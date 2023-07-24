caf94d518e48c0058d3ea66f89662346cb9c4beb
Riya Roy <riyaroy2086@gmail.com>
2023-07-24 23:47:43 +0530

    few changes

---
+++ Contest/LC Biweekly Contest 109/2787. Ways to Express an Integer as Sum of Powers.cpp
@@ -0,0 +1,55 @@
+Given two positive integers n and x.
+
+Return the number of ways n can be expressed as the sum of the xth power of unique positive integers, in other words, the number of sets of unique integers [n1, n2, ..., nk] where n = n1x + n2x + ... + nkx.
+
+Since the result can be very large, return it modulo 109 + 7.
+
+For example, if n = 160 and x = 3, one way to express n is n = 23 + 33 + 53.
+
+ 
+
+Example 1:
+
+Input: n = 10, x = 2
+Output: 1
+Explanation: We can express n as the following: n = 32 + 12 = 10.
+It can be shown that it is the only way to express 10 as the sum of the 2nd power of unique integers.
+Example 2:
+
+Input: n = 4, x = 1
+Output: 2
+Explanation: We can express n in the following ways:
+- n = 41 = 4.
+- n = 31 + 11 = 4.
+ 
+
+Constraints:
+
+1 <= n <= 300
+1 <= x <= 5
+
+// -------------------SOLUTION -----------------
+
+
+class Solution {
+ public:
+  int numberOfWays(int n, int x) {
+    constexpr int kMod = 1'000'000'007;
+    // dp[i] := # of ways to express i
+    vector<int> dp(n + 1);
+    int ax;  // a^x
+
+    dp[0] = 1;
+
+    for (int a = 1; (ax = pow(a, x)) <= n; ++a)
+      for (int i = n; i >= ax; --i) {
+        dp[i] += dp[i - ax];
+        dp[i] %= kMod;
+      }
+
+    return dp[n];
+  }
+};
+
+TC -> O(N log N)
+SC-> O(N)
---
+++ Contest/LC Weekly Contest 355/2789. Largest Element in an Array after Merge Operations.cpp
@@ -0,0 +1,49 @@
+You are given a 0-indexed array nums consisting of positive integers.
+
+You can do the following operation on the array any number of times:
+
+Choose an integer i such that 0 <= i < nums.length - 1 and nums[i] <= nums[i + 1]. Replace the element nums[i + 1] with nums[i] + nums[i + 1] and delete the element nums[i] from the array.
+Return the value of the largest element that you can possibly obtain in the final array.
+
+ 
+
+Example 1:
+
+Input: nums = [2,3,7,9,3]
+Output: 21
+Explanation: We can apply the following operations on the array:
+- Choose i = 0. The resulting array will be nums = [5,7,9,3].
+- Choose i = 1. The resulting array will be nums = [5,16,3].
+- Choose i = 0. The resulting array will be nums = [21,3].
+The largest element in the final array is 21. It can be shown that we cannot obtain a larger element.
+Example 2:
+
+Input: nums = [5,3,3]
+Output: 11
+Explanation: We can do the following operations on the array:
+- Choose i = 1. The resulting array will be nums = [5,6].
+- Choose i = 0. The resulting array will be nums = [11].
+There is only one element in the final array, which is 11.
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+1 <= nums[i] <= 106
+
+// -------------------SOLUTION -----------------
+
+class Solution {
+public:
+    long long maxArrayValue(vector<int>& nums) {
+        long long ans = nums.back();
+
+        for (int i = nums.size() - 2; i >= 0; --i)
+          if (nums[i] > ans)
+            ans = nums[i];
+          else
+            ans += nums[i];
+
+        return ans;
+    }
+};
