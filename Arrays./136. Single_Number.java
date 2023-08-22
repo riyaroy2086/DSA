8cb12fa2f2fadb6be4efe186dd6fa9414c3efc48
Archisman Mandal <archi.rider14@gmail.com>
2023-08-23 00:48:22 +0530

    LC

---
+++ Arrays./136. Single_Number.java
@@ -0,0 +1,38 @@
+//https://leetcode.com/problems/single-number/
+
+/**
+Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+Example 1:
+
+Input: nums = [2,2,1]
+Output: 1
+Example 2:
+
+Input: nums = [4,1,2,1,2]
+Output: 4
+Example 3:
+
+Input: nums = [1]
+Output: 1
+*/
+
+//-----------------------------------------------------------------code here---------------------------------------------------------------------------------//
+
+class Solution {
+    public int singleNumber(int[] nums) {
+
+        Arrays.sort(nums);
+
+        for(int i=0;i<nums.length-1;i++)
+        {
+            if(i==0 && nums[i]!=nums[i+1]) return nums[i];
+
+            else if(nums[i]!=nums[i+1] && nums[i]!=nums[i-1]) return nums[i];
+        }
+
+        return nums[nums.length-1];
+        
+    }
+}
