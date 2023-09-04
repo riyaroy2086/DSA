276ba437beeddf3599f3110f1100a574999ded9c
Archisman Mandal <archi.rider14@gmail.com>
2023-09-04 15:52:07 +0530

    LC

---
+++ Linked List 1.0/1669. Merge_In_Between_Linked_Lists.java
@@ -0,0 +1,54 @@
+//https://leetcode.com/problems/merge-in-between-linked-lists/
+
+/**
+You are given two linked lists: list1 and list2 of sizes n and m respectively.
+Remove list1's nodes from the ath node to the bth node, and put list2 in their place.
+The blue edges and nodes in the following figure indicate the result:
+Build the result list and return its head.
+
+Example 1:
+
+Input: list1 = [0,1,2,3,4,5], a = 3, b = 4, list2 = [1000000,1000001,1000002]
+Output: [0,1,2,1000000,1000001,1000002,5]
+Explanation: We remove the nodes 3 and 4 and put the entire list2 in their place. The blue edges and nodes in the above figure indicate the result.
+
+
+Example 2:
+
+Input: list1 = [0,1,2,3,4,5,6], a = 2, b = 5, list2 = [1000000,1000001,1000002,1000003,1000004]
+Output: [0,1,1000000,1000001,1000002,1000003,1000004,6]
+Explanation: The blue edges and nodes in the above figure indicate the result.
+*/
+
+//------------------------------------------------------------------code here--------------------------------------------------------------------------------//
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeInBetween(ListNode list1, int a, int b, ListNode list2) {
+
+        ListNode ptr1=list1;
+        ListNode ptr2=list1;
+
+        for(int i=0;i<a-1;i++) ptr1=ptr1.next;
+
+        for(int j=0;j<b+1;j++) ptr2=ptr2.next;
+
+        ptr1.next=list2;
+
+        while(list2.next!=null) list2=list2.next;
+
+        list2.next=ptr2;
+
+        return list1;
+        
+    }
+}
